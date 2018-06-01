3afe6608f3e7d2105e62d152483371aed315f2a3
gc-sho <nenad.jakovljevic@gamecredits.com>
2018-06-01 13:12:51 +0200

    session file removed

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-from gc_test_flask.session import remove_file
 from gc_test_flask import application
 
 import atexit
 
 if __name__ == '__main__':
-    # On force quit, remove session files
-    atexit.register(remove_file, [application.config['TMP_PATH'], application.config['VERIFY_PATH']])
     application.run(application.config['HOST'],  application.config['PORT'])
